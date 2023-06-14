e3511909fd214c7b27e79e27ca9c436e4a2754d8
Victor <victormilitao@gmail.com>
2023-06-14 12:41:39 -0300

    adicionando um pouco de estilização e criando a home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserStocksComponent } from './pages/user-stocks/user-stocks.component';
+import { HomeComponentComponent } from './pages/home/home-component/home-component.component';
 
 const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponentComponent,
+  },
   {
     path: "user-stocks",
     component: UserStocksComponent,
