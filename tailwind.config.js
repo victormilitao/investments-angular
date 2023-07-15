9e537571dfe74f249ba92074e7a976ed4d9a68f4
Victor <victormilitao@gmail.com>
2023-07-14 21:35:57 -0300

    adicionando componente para importação da planilha. configuração de cores do design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'orange-500': '#FF9000'
+        'ds-white': {
+          DEFAULT: 'var(--ds-white-50)',
+          100: 'var(--ds-white-100)',
+        },
+        'ds-black': {
+          DEFAULT: 'var(--ds-black-500)',
+          500: 'var(--ds-black-500)',
+          400: 'var(--ds-black-400)',
+          300: 'var(--ds-black-300)',
+        },
+        'ds-orange' : {
+          DEFAULT: 'var(--ds-orange-500)',
+          500: 'var(--ds-orange-500)',
+        },
       }
     },
   },
   plugins: [],
 }
-
